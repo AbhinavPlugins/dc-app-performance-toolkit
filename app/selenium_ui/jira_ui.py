52841287d2327c3420f47324ad091adc54348a78
AbhinavPlugins <abhinavojha93@outlook.com>
2021-05-11 03:57:38 +0530

    - App specific test cases

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -63,6 +63,14 @@ Refer to `app/selenium_ui/jira/modules.py` for examples.
 # def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
 #     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
 
+def test_1_app_specific_action_misc_config(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action_misc_config(jira_webdriver, jira_datasets)
+
+def test_1_app_specific_action_misc_logwork(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action_misc_logwork(jira_webdriver, jira_datasets)
+
+def test_1_app_specific_action_misc_report(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action_misc_report(jira_webdriver, jira_datasets)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
